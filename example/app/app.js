6304cc2b80ee1f5dc17f66278b07acf9d6225316
Minh Tran <vn38minhtran@gmail.com>
2015-10-13 14:40:02 +0700

    Initial

---
+++ example/app/app.js
@@ -0,0 +1,35 @@
+import 'babel-core/polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './components/Header.js';
+import Footer from './components/Footer.js';
+import Component from 'react-tooltip-component';
+
+import './bower_components/bootstrap-customize/css/bootstrap.css';
+import './assets/styles/app.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='layout-page'>
+        <Header/>
+        <main className='layout-main'>
+          <div className='container'>
+            <Component/>
+          </div>
+        </main>
+        <Footer/>
+      </div>
+    );
+  }
+}
+
+function run() {
+  ReactDOM.render(<App />, document.getElementById('app'));
+}
+
+if (window.addEventListener) {
+  window.addEventListener('DOMContentLoaded', run);
+} else {
+  window.attachEvent('onload', run);
+}
---
+++ src/Component.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Component extends React.Component {
+  render() {
+    return (
+      <div>Component</div>
+    );
+  }
+}
